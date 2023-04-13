77609c88c96a93220ca1987ee065688bccde249d
Aarsh30 <patelaarsh3011@gmail.com>
2023-04-13 11:51:28 +0530

    new fixed

--- components/footer.js
+++ components/footer.js
@@ -3,8 +3,8 @@ import { Box } from '@chakra-ui/react'
 const Footer = () => {
   return (
     <Box align="center" opacity={0.4} fontSize="sm">
-      &copy; {new Date().getFullYear()} Aarsh Patel .
-      This website Inspiration taken from <a href='https://www.craftz.dog/'>Takuya Matsuyama's website</a>.
+      {/* &copy; {new Date().getFullYear()} Aarsh Patel . */}
+      {/* This website Inspiration taken from <a href='https://www.craftz.dog/'>Takuya Matsuyama's website</a>. */}
     </Box>
   )
 }
